79c0f32ef2b631575649c83cc01ca6671f336f3b
wvanheemstra <wvanheemstra@users.noreply.github.com>
2023-10-30 16:41:46 +0100

    Update FooServlet.java

--- src/main/java/com/mycompany/app/FooServlet.java
+++ src/main/java/com/mycompany/app/FooServlet.java
@@ -5,8 +5,9 @@ import javax.servlet.http.*;
 public class FooServlet extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
   throws ServletException, IOException {
+    String cardSecurityCode = "12345";
     // User provided value
-    String cardSecurityCode = request.getParameter("cardSecurityCode");
+    // String cardSecurityCode = request.getParameter("cardSecurityCode");
     Runtime.getRuntime().exec("validateCode.sh " + cardSecurityCode);
   }
 }
